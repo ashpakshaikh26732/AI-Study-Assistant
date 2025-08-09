0db80b378fa8792b1fd8ac8d70e599c237b1899a
ashpakshaikh26732 <ashpakshaikh26732@gmail.com>
2025-08-09 05:14:18 +0000

    added cleaning_fn and completed text_cleaner.py module

---
+++ src/Preprocessing/text_cleaner.py.py
@@ -0,0 +1,15 @@
+import re
+def clearning_fn(text) : 
+    """
+    Cleans raw extracted text by performing a series of normalization tasks.
+
+    Args:
+        text (str): The raw text extracted from a document.
+
+    Returns:
+        str: The cleaned and normalized text.
+    """
+    text = re.sub(r'\s+' , " " , text)
+    text = text.strip() 
+    cleaned_text = re.sub(r'(\w+)-\s*\n\s*(\w+)', r'\1\2', text)
+    return cleaned_text 
